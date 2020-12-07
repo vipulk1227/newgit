f1f76e035b9c7fa5560a73aedc0ca2d9b1c42352
vipulk1227 <vipulk1227@gmail.com>
2020-12-07 17:19:15 +0530

    this is fourth class

---
+++ src/project/Class4.java
@@ -0,0 +1,11 @@
+package project;
+
+public class Class4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is the fourth class");
+	}
+
+}
